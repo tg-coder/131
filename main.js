fcf26eaebe3556e684114f5623bd148c7294543c
tg-coder <76582848+tg-coder@users.noreply.github.com>
2021-10-11 16:44:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,24 @@ function setup()
 {
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDectector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects . . .";
+}
+img = "";
+status = "";
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
 function preload()
 {
@@ -9,5 +27,15 @@ function preload()
 }
 function draw()
 {
-    Image(img,0,0,640,420);
+    image(img,0,0,640,420);
+    fill("#62ff00");
+    text("dog", 45, 75);
+    noFill();
+    stroke("cyan");
+    rect(30, 60,450, 350);
+    fill("#8cb4f5");
+    text("cat", 320, 120);
+    noFill();
+    stroke("cyan");
+    rect(300, 90, 300, 300);
 }
