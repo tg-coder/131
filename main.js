79817c3b83a7d710e79ecd07a4e8ace5e583e593
tg-coder <76582848+tg-coder@users.noreply.github.com>
2021-10-25 16:54:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,50 @@
-function setup()
-{
-    canvas = createCanvas(640,420);
-    canvas.center();
-    objectDetector = ml5.objectDectector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects . . .";
-}
 img = "";
-status = "";
-function modelLoaded()
-{
-    console.log("Model Loaded");
-    status = true;
-    objectDetector.detect(img, gotResult);
-}
-function gotResult(error, results)
-{
-    if(error)
-    {
-        console.log(error);
-    }
-    console.log(results);
-}
-function preload()
-{
-    img = loadImage('dog_cat.jpg');
-}
-function draw()
-{
-    image(img,0,0,640,420);
-    fill("#62ff00");
-    text("dog", 45, 75);
-    noFill();
-    stroke("cyan");
-    rect(30, 60,450, 350);
-    fill("#8cb4f5");
-    text("cat", 320, 120);
-    noFill();
-    stroke("cyan");
-    rect(300, 90, 300, 300);
-}
+ status = "";
+objects = [];
+ function preload()
+ {
+      img = loadImage('dog_cat.jpg');
+     } 
+     function setup() 
+     { 
+         canvas = createCanvas(380, 380); 
+         canvas.center(); 
+         video = createCapture(VIDEO);
+         video.hide();
+         objectDetector = ml5.objectDetector('cocossd', modelLoaded); 
+         document.getElementById("status").innerHTML = "Status : Detecting Objects"; 
+        } 
+        function modelLoaded() 
+        { 
+            console.log("Model Loaded!") 
+            status = true;
+            } 
+            function gotResults(error, results) 
+            { 
+                if (error) { console.log(error);
+                 } 
+                 console.log(results);
+                 objects = results;
+                }
+                 function draw() 
+                 { 
+                     image(video, 0, 0, 380, 380); 
+                     if(status!="")
+                     {
+                         r = random(255);
+                         g = random(255);
+                         b = random(255);
+                         objectDetector.detect(video, gotResults);
+                         for(i = 0;i<objects.length;i++)
+                         {
+                             document.getElementById("status").innerHTML = "Status: Object Detected!";
+                             document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+                             fill(r,g,b);
+                             percent = floor(objects[i].confidence *100);
+                             text(objects[i].label + ""+percent+"%" ,objects[i].x,objects[i].y);
+                             noFill();
+                             stroke(r,g,b);
+                             rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+                         }
+                     }
+                    }
